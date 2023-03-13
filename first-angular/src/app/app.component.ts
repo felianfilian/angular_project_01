26510b16e7f2ad4d51ff17626b475b46ba2214fe
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-03-13 09:49:50 +0100

    individual variables

--- first-angular/src/app/app.component.ts
+++ first-angular/src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-angular';
-  name = 'darian';
 
   GetHero() {
     alert('Warrior');
--- first-angular/src/app/hero-card/hero-card.component.ts
+++ first-angular/src/app/hero-card/hero-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-card',
   templateUrl: './hero-card.component.html',
-  styleUrls: ['./hero-card.component.scss']
+  styleUrls: ['./hero-card.component.scss'],
 })
 export class HeroCardComponent {
-
+  @Input() name: string = '';
 }
